0b40d8d36dce5ae2ed8ec9a5584c358859d8c589
Anthony S.J. Henry <30806798+AnthonySJHenry@users.noreply.github.com>
2025-11-08 18:09:16 -0500

    have fun.. build something

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import glabIcon from "./assets/glab.svg";
 import antEllipse from "./assets/Anthony.png";
 import "./App.css";
+
+function Hello({ name }: { name: string }) {
+  return (
+    <p>
+      <code>Hello, {name}!</code>
+    </p>
+  );
+}
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,28 +18,27 @@ function App() {
     <>
       <div>
         <a href="https://github.com/AnthonySJHenry" target="_blank">
-          <img src={antEllipse} className="logo" alt="Vite logo" />
+          <img src={antEllipse} className="logo" alt="Anthony logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://gitlab.com/AnthonySJHenry" target="_blank">
+          <img src={glabIcon} className="logo" alt="Glab logo" />
         </a>
       </div>
-      <h1>Anthony + React</h1>
+      <h1>AnthonySJHenry</h1>
       <div
-        onClick={() =>
-          setCount((count) => count * 2) > alert("You clicked me!")
-        }
+        onClick={() => {
+          setCount((count) => count * Math.floor(Math.random() * 10) + 1);
+          alert("You clicked me! You've multipled your count");
+        }}
       >
-        <p className="read-the-docs">Click on me</p>
+        <p className="read-the-docs">Click on stuff</p>
       </div>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          <code>Hello, World!</code>
-        </p>
       </div>
+      <Hello name="Anthony" />
     </>
   );
 }
